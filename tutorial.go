9c6953cd8cbab1f590fc52f9be5a9d448759ed97
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-05-17 11:58:43 +0530

    done with variables and data types

--- tutorial.go
+++ tutorial.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello Sudoaptrana to Go")
+
+	//var name type is how we are going to declare varibales
+	var name string
+	name = "Tim"
+	name = "rana"
+	var number uint8 = 123
+	number = number + 78
+	fmt.Println("Hello Sudoaptrana to Go", name, number)
 }
